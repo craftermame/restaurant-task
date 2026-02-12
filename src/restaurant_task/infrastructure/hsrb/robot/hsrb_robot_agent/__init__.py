baf1c047d726a73c530bf5431aab5672b70d2964
craftermame <kaiji.owada.256@gmail.com>
2026-02-12 12:48:59 +0900

    refactor: delete a line

--- src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/__init__.py
+++ src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/__init__.py
@@ -5,7 +5,6 @@ from restaurant_task.domain.models.robot.i_robot_agent import IRobotAgent
 rclpy.init()
 from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent.client.supertonic import supertonic_client
 
-
 class HSRBRobotAgent(IRobotAgent):
     async def speak(self, sentence):
         await supertonic_client.execute(sentence)
