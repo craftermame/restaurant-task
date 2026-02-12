9700c9b5c491c3745b7becdbf962ec459c9d5bd9
craftermame <kaiji.owada.256@gmail.com>
2026-02-13 05:33:30 +0900

    feat: config

--- src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/config.py
+++ src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/config.py
@@ -5,6 +5,7 @@ class MsgConfig:
     WHEEL_ODOM: Final = "/omni_base_controller/wheel_odom"
     GRIPPER: Final = "/gripper_controller/grasp"
     LASER_SCAN: Final = "/scan"
+    MOVE_TO_POSE: Final = "/hsrb/move_to_pose"
 
 class Config:
     msg = MsgConfig()
