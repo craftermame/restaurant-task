7423c98155c8f121f5be67856d000a9e9fca91e1
craftermame <kaiji.owada.256@gmail.com>
2026-02-12 14:54:52 +0900

    feat: test robot agent

---
+++ test/robot/test_hsrb.py
@@ -0,0 +1,12 @@
+from restaurant_task.config import config
+
+from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent import hsrb
+
+async def test_speak():
+    await hsrb.speak("This is the sample sentence.")
+
+async def test_ask():
+    await hsrb.speak("Please just answer coffee for following question.")
+
+    order = await hsrb.ask(config.message.ORDER_QUESTION)
+    assert order == "coffee"
--- test/robot/test_hsrb_speak.py
+++
@@ -1,7 +0,0 @@
-from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent import hsrb
-
-def test_hsrb_speak():
-    hsrb.speak("This is the sample sentence.")
-
-if __name__ == "__main__":
-    test_hsrb_speak()
