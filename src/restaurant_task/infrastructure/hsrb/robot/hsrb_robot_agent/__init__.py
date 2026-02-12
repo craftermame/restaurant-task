2bde4bc7ef227ddcc4246b05337965c6c6f9f618
craftermame <kaiji.owada.256@gmail.com>
2026-02-12 14:34:09 +0900

    feat: add vosk client

--- src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/__init__.py
+++ src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/__init__.py
@@ -1,9 +1,14 @@
 from restaurant_task.domain.models.robot.i_robot_agent import IRobotAgent
 
 from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent.client.supertonic import supertonic_client
+from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent.client.vosk import vosk_client
 
 class HSRBRobotAgent(IRobotAgent):
     async def speak(self, sentence):
         await supertonic_client.execute(sentence)
+
+    async def ask(self, question, duration_sec = 4):
+        await self.speak(question)
+        return await vosk_client.execute(duration_sec)
 
 hsrb = HSRBRobotAgent()
---
+++ src/restaurant_task/infrastructure/hsrb/robot/hsrb_robot_agent/client/vosk.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from sobits_interfaces.action import SpeechRecognition
+
+from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent import utils
+
+class VoskClient(Node):
+    @utils.with_rclpy
+    def __init__(self):
+        super().__init__("vosk")
+        self.action_client = \
+            ActionClient(self, SpeechRecognition, "/speech_recognition")
+
+        if not self.action_client.wait_for_server(timeout_sec=3.0):
+            raise RuntimeError("Cannot connect to vosk server.")
+
+    async def execute(self, timeout_sec: int):
+        goal_msg = SpeechRecognition.Goal()
+        goal_msg.timeout_sec = timeout_sec
+        goal_msg.silent_mode = False
+        goal_msg.feedback_rate = 0.5
+
+        # Goal送信
+        send_goal_future = self.action_client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+
+        # ゴールの待機
+        goal_handle = send_goal_future.result()
+        if not (goal_handle and goal_handle.accepted):
+            self.get_logger().error('Goal was rejected.')
+            return None
+        get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, get_result_future)
+
+        result = get_result_future.result()
+
+        return result.result.result_text
+
+vosk_client = VoskClient()
