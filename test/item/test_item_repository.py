73dad37f4c120de1bf912b6d383ef3d45f52669c
craftermame <kaiji.owada.256@gmail.com>
2026-02-12 23:44:36 +0900

    feat: test item repository

---
+++ test/item/test_item_repository.py
@@ -0,0 +1,22 @@
+from restaurant_task.domain.models.item.item import Item, ItemId, ItemIdentity, Category
+
+from restaurant_task.infrastructure.csvdb.item.csvdb_item_repository import CSVDBItemRepository
+
+async def test_csvdb_item_repository():
+    csvdb_item_repository = CSVDBItemRepository()
+
+    item_id = ItemId("coffee")
+    item = await csvdb_item_repository.find_by_id(item_id)
+
+    expected_item = Item(
+        ItemIdentity(ItemId("coffee")),
+        Category("drink")
+    )
+
+    assert item == expected_item
+
+    ids = await csvdb_item_repository.get_all_ids()
+    ids_str = ["caramel_corn", "cookie", "potato_chips", "cola", "green_tea", "coffee"]
+    excepted_ids = [ItemId(item_id_str) for item_id_str in ids_str]
+
+    assert set(ids) == set(excepted_ids)
