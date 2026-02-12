6a8dc65ea71ca42cf8caf983ba19aa656af3b8b3
craftermame <kaiji.owada.256@gmail.com>
2026-02-12 14:34:23 +0900

    test: vosk client

--- src/test.py
+++ src/test.py
@@ -3,7 +3,8 @@ import asyncio
 from restaurant_task.infrastructure.hsrb.robot.hsrb_robot_agent import hsrb
 
 async def test_hsrb_speak():
-    await hsrb.speak("This is the sample sentence.")
+    raw_order = await hsrb.ask("What's your order?")
+    print(raw_order)
 
 if __name__ == "__main__":
     asyncio.run(test_hsrb_speak())
